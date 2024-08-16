6125617bdd4b29c055b6682945ceacccbccdc0dd
sajjad isvand <sajjadzibaee102@gmail.com>
2024-08-16 21:24:36 +0330

    refactor: Improve UI styling and structure in nerkhYab app

--- src/nerkhYab/app.nerkh.tsx
+++ src/nerkhYab/app.nerkh.tsx
@@ -47,51 +47,52 @@ function App() {
 
   return (
     <>
-      <div className="h-screen w-screen">
-        <div className="flex flex-col h-full justify-around items-center moveable">
-          <div className="flex flex-row items-center justify-around  w-full">
-            <div>
-              {loading ? (
-                <div className="h-10 w-10 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700"></div>
-              ) : (
-                <img src={`/public/assets/countries/usa.png`} />
-              )}
-            </div>
-            <div className="flex flex-col w-32 justify items-end">
-              {loading ? (
-                <div className="h-6 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-20 mb-2.5"></div>
-              ) : (
-                <h3 className="text-[1.5rem] text-[#737373] dark:text-[#eee]">
-                  {data.name}
-                </h3>
-              )}
-              {/* <h6 className="text-[.7rem]  text-[#858585]">
-                پووف! خیلی گرونه 😁
-              </h6> */}
-            </div>
-          </div>
-          <div className="flex flex-row items-center w-full">
-            <div className="flex flex-col ml-7">
+      <div className="h-screen w-screen overflow-hidden">
+        <div className="moveable py-3 px-0 h-full">
+          <div className="flex flex-col gap-6 h-full justify-around items-center">
+            <div className="flex flex-row items-center justify-around  w-full flex-wrap gap-2">
               <div>
-                {/* <p className="text-2xl text-[#525252]">59,800</p> */}
                 {loading ? (
-                  <div className="h-2 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-10 mb-2.5"></div>
+                  <div className="h-10 w-10 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700"></div>
                 ) : (
-                  <p className="text-2xl text-[#525252] dark:text-[#d3d3d3]">
-                    {data.todyPrice.toLocaleString()}
-                  </p>
+                  <img src={`/public/assets/countries/usa.png`} />
                 )}
               </div>
-              <div>
-                {/* <p className="text-xs text-gray-500">1 USD</p> */}
+              <div className="flex flex-col w-32 justify items-end truncate">
                 {loading ? (
-                  <div className="h-2 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-5 mb-2.5"></div>
+                  <div className="h-5 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-20 mb-2.5"></div>
                 ) : (
-                  <p className="text-xs text-gray-500 dark:text-[#cbc9c9]">
-                    1 USD
-                  </p>
+                  <h3 className="text-[1.2rem] text-[#737373] dark:text-[#eee] truncate">
+                    {data.name}
+                  </h3>
                 )}
+                {/* <h6 className="text-[.7rem]  text-[#858585]">
+                  پووف! خیلی گرونه 😁
+                </h6> */}
               </div>
+            </div>
+            <div className="flex flex-row w-full justify-around items-center">
+              <div className="flex flex-col">
+                <div>
+                  {loading ? (
+                    <div className="h-2 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-10 mb-2.5"></div>
+                  ) : (
+                    <p className="text-2xl text-[#525252] dark:text-[#d3d3d3]">
+                      {data.todyPrice.toLocaleString()}
+                    </p>
+                  )}
+                </div>
+                <div>
+                  {loading ? (
+                    <div className="h-2 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-5 mb-2.5"></div>
+                  ) : (
+                    <p className="text-xs text-gray-500 dark:text-[#cbc9c9]">
+                      1 USD
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div style={{ visibility: 'hidden' }}>aaaaaaaaa</div>
             </div>
           </div>
         </div>
