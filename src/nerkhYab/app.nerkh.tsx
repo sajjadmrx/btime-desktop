9d03f40ae2481b68ae494754c66bb1c329ab4f99
sajjad isvand <sajjadzibaee102@gmail.com>
2024-08-24 00:18:35 +0330

    refactor(nerkhYab): Refactor currency info display and add dynamic color extraction

--- src/nerkhYab/app.nerkh.tsx
+++ src/nerkhYab/app.nerkh.tsx
@@ -1,10 +1,7 @@
-import { useEffect, useState } from 'react'
-import { CurrencyData, getRateByCurrency } from '../api/nerkh.api'
+import { useEffect } from 'react'
+import { CurrencyInfoCard } from './components/currencyCard.component'
 
 function App() {
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState<CurrencyData>()
-
   useEffect(() => {
     const handleColorSchemeChange = (e) => {
       document.documentElement.classList.remove('dark')
@@ -27,77 +24,23 @@ function App() {
     }
   }, [])
 
-  useEffect(() => {
-    function fetchData() {
-      getRateByCurrency('usd').then((data) => {
-        if (data) {
-          setData(data)
-          setLoading(false)
-        }
-      })
-    }
-
-    fetchData()
-
-    return () => {
-      setData(undefined)
-      setLoading(true)
-    }
-  }, [])
-
   return (
-    <>
-      <div className="h-screen w-screen overflow-hidden">
-        <div className="moveable py-3 px-0 h-full">
-          <div className="flex flex-col gap-6 h-full justify-around items-center">
-            <div className="flex flex-row items-center justify-around  w-full flex-wrap gap-2">
-              <div>
-                {loading ? (
-                  <div className="h-10 w-10 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700"></div>
-                ) : (
-                  <img src={`./assets/countries/usa.png`} />
-                )}
-              </div>
-              <div className="flex flex-col w-32 justify items-end truncate">
-                {loading ? (
-                  <div className="h-5 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-20 mb-2.5"></div>
-                ) : (
-                  <h3 className="text-[1.2rem]  text-gray-600 text-gray-trasnparent dark:text-[#eee] truncate">
-                    {data.name}
-                  </h3>
-                )}
-                {/* <h6 className="text-[.7rem]  text-[#858585]">
-                  پووف! خیلی گرونه 😁
-                </h6> */}
-              </div>
-            </div>
-            <div className="flex flex-row w-full justify-around items-center">
-              <div className="flex flex-col">
-                <div>
-                  {loading ? (
-                    <div className="h-2 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-10 mb-2.5"></div>
-                  ) : (
-                    <p className="text-[1.2rem]  text-gray-600 text-gray-trasnparent dark:text-[#d3d3d3]">
-                      {data.todyPrice.toLocaleString()}
-                    </p>
-                  )}
-                </div>
-                <div>
-                  {loading ? (
-                    <div className="h-2 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-5 mb-2.5"></div>
-                  ) : (
-                    <p className="text-xs text-gray-600 text-gray-trasnparent dark:text-[#cbc9c9]">
-                      1 USD
-                    </p>
-                  )}
-                </div>
-              </div>
-              <div style={{ visibility: 'hidden' }}>aaaaaaaaa</div>
-            </div>
+    <div className="h-screen w-screen overflow-hidden" dir="rtl">
+      <div className="moveable py-3 px-0 h-full" dir="rtl">
+        <div
+          className="flex flex-col gap-6 h-full justify-around items-center"
+          dir="rtl"
+        >
+          <div
+            className="h-full flex flex-col items-center justify-around w-full px-2 flex-wrap gap-5"
+            style={{ maxHeight: '80vh' }}
+            dir="rtl"
+          >
+            <CurrencyInfoCard />
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
---
+++ src/nerkhYab/components/currencyCard.component.tsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from 'react'
+import { CurrencyData, getRateByCurrency } from '../../api/nerkh.api'
+import { widgetKey } from 'electron/store'
+import { extractMainColorFromImage } from '../../utils/colorUtils'
+
+export function CurrencyInfoCard() {
+  const [loading, setLoading] = useState(true)
+  const [currency, setCurrency] = useState<string>()
+  const [imgColor, setImgColor] = useState<string>()
+  const [currencyData, setCurrencyData] = useState<CurrencyData>(null)
+
+  useEffect(() => {
+    const currencyStore = window.store.get('NerkhYab' as widgetKey.NerkhYab)
+    setCurrency(currencyStore.currencies[0])
+
+    async function fetchData() {
+      const data = await getRateByCurrency(currencyStore.currencies[0])
+      if (data) {
+        setCurrencyData(data)
+        setLoading(false)
+      }
+    }
+
+    fetchData()
+
+    return () => {
+      setCurrencyData(undefined)
+      setLoading(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (currencyData && currency) {
+      extractMainColorFromImage(currencyData.icon).then((color) => {
+        setImgColor(color)
+      })
+    }
+  }, [currencyData, currency])
+
+  return (
+    <div
+      className="flex flex-col gap-6 h-full justify-around items-center"
+      dir="ltr"
+    >
+      <div className="flex flex-row items-center justify-around  w-full flex-wrap gap-2">
+        <div>
+          {loading ? (
+            <div className="h-10 w-10 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700"></div>
+          ) : (
+            <div
+              className={`w-10 h-10 relative flex rounded-full overflow-hidden`}
+              style={{
+                backdropFilter: 'blur(100px)',
+                boxShadow: `0px 0px 4px 1px ${imgColor}`,
+              }}
+            >
+              <img src={currencyData.icon} className="object-center" />
+            </div>
+          )}
+        </div>
+        <div className="flex flex-col w-32 justify items-end truncate">
+          {loading ? (
+            <div className="h-5 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-20 mb-2.5"></div>
+          ) : (
+            <h3
+              className="text-[1.1rem] font-normal  text-gray-600 text-gray-trasnparent dark:text-[#eee] truncate w-32"
+              dir="rtl"
+            >
+              {currencyData.name}
+            </h3>
+          )}
+        </div>
+      </div>
+      <div className="flex flex-row w-full justify-around items-center">
+        <div className="flex flex-col">
+          <div>
+            {loading ? (
+              <div className="h-2 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-10 mb-2.5"></div>
+            ) : (
+              <p className="text-[1.2rem]  text-gray-600 text-gray-trasnparent dark:text-[#d3d3d3]">
+                {currencyData.todyPrice.toLocaleString()}
+              </p>
+            )}
+          </div>
+          <div>
+            {loading ? (
+              <div className="h-2 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-5 mb-2.5"></div>
+            ) : (
+              <p className="text-xs font-light text-gray-600 text-gray-trasnparent dark:text-[#cbc9c9]">
+                1 {currency.toUpperCase()}
+              </p>
+            )}
+          </div>
+        </div>
+        <div style={{ visibility: 'hidden' }}>aaaaaaaaa</div>
+      </div>
+    </div>
+  )
+}
