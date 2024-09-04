83f090586e151247604df4fdfc12ca306cc2c741
sajjad isvand <sajjadzibaee102@gmail.com>
2024-09-04 23:11:53 +0330

    refactor(nerkhyab): improved UI

--- src/nerkhYab/components/currencyCard.component.tsx
+++ src/nerkhYab/components/currencyCard.component.tsx
@@ -42,34 +42,37 @@ export function CurrencyInfoCard() {
       className="flex flex-col gap-6 h-full justify-around items-center relative"
       dir="ltr"
     >
-      <div className="flex flex-row items-center justify-around  w-full flex-wrap gap-2">
-        <div
-          className={`
-           w-24 h-24 absolute rounded-full z-0 -left-7 blur-lg`}
-          style={{
-            backgroundImage: `radial-gradient(50% 50% at 50% 50%, ${imgColor} 35%, ${imgColor + '00'} 30%)`,
-          }}
-        ></div>
+      <div className="flex flex-row items-center justify-around  w-full flex-wrap space-x-1">
         <div>
           {loading ? (
             <div className="h-10 w-10 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700"></div>
           ) : (
             <div
-              className={`w-10 h-10 relative flex rounded-full overflow-hidden`}
+              className={`w-10 h-10 relative flex rounded-full`}
               style={{
                 backdropFilter: 'blur(100px)',
               }}
             >
-              <img src={currencyData.icon} className="object-cover" />
+              <div
+                className={`
+           w-32 h-10 absolute rounded-full z-0 -left-10 blur-lg `}
+                style={{
+                  backgroundImage: `radial-gradient(50% 50% at 50% 50%, ${imgColor} 35%, ${imgColor + '00'} 50%)`,
+                }}
+              ></div>
+              <img
+                src={currencyData.icon}
+                className="object-cover z-10 rounded-full"
+              />
             </div>
           )}
         </div>
-        <div className="flex flex-col w-32 justify items-end truncate">
+        <div className="flex flex-col w-32 justify items-end truncate ">
           {loading ? (
             <div className="h-5 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-20 mb-2.5"></div>
           ) : (
             <h3
-              className="text-[1.1rem] font-normal  text-gray-600 text-gray-trasnparent dark:text-[#eee] truncate w-32"
+              className="lg:text-[1.1rem] sm:text-sm font-normal  text-gray-600 text-gray-trasnparent dark:text-[#eee] truncate w-32"
               dir="rtl"
             >
               {currencyData.name}
@@ -83,7 +86,7 @@ export function CurrencyInfoCard() {
             {loading ? (
               <div className="h-2 animate-pulse bg-gray-200 rounded-full dark:bg-gray-700 w-10 mb-2.5"></div>
             ) : (
-              <p className="text-[1.2rem]  text-gray-600 text-gray-trasnparent dark:text-[#d3d3d3]">
+              <p className="lg:text-[1.2rem] sm:text-sm md:text-[.9rem]  text-gray-600 text-gray-trasnparent dark:text-[#d3d3d3]">
                 {currencyData.todyPrice.toLocaleString()}
               </p>
             )}
