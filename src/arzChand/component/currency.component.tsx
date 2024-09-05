051c3e76c5c4bdd5f983155fa9192c7ce71e2356
sajjad isvand <sajjadzibaee102@gmail.com>
2024-09-06 01:03:15 +0330

    refactor(arzChand): optimize currency display

--- src/arzChand/component/currency.component.tsx
+++ src/arzChand/component/currency.component.tsx
@@ -28,15 +28,21 @@ export function CurrencyComponent({ currency }: Prop) {
         <div className="text-[.9rem] flex flex-col text-gray-600 text-gray-trasnparent  dark:text-[#eee] truncate">
           <div className="flex-1 flex flex-row w-52 items-center justify-end mt-1 p-2 rounded-full truncate ">
             <div className="w-10 flex justify-center" dir="rtl">
-              <div
-                className={`w-8 h-8 relative  flex rounded-full overflow-hidden`}
-                style={{
-                  backdropFilter: 'blur(100px)',
-                  boxShadow: `0px 0px 4px 1px ${imgColor}`,
-                }}
-              >
+              <div className={`w-8 h-8 relative flex rounded-full`}>
                 {currency ? (
-                  <img src={currency.icon} className="object-cover" />
+                  <>
+                    <div
+                      className="absolute inset-0 h-8 w-8 rounded-full z-0"
+                      style={{
+                        backdropFilter: 'blur(100px)',
+                        boxShadow: `0px 0px 5px 3px ${imgColor}`,
+                      }}
+                    ></div>
+                    <img
+                      src={currency.icon}
+                      className="object-cover rounded-full w-8 h-8 z-10"
+                    />
+                  </>
                 ) : (
                   <div className="animate-pulse bg-gray-200 w-full h-full"></div>
                 )}
@@ -48,7 +54,7 @@ export function CurrencyComponent({ currency }: Prop) {
                   {currency.name}
                 </p>
               ) : (
-                <p className="mr-3 items-center truncate w-20 h-3 animate-pulse bg-gray-200/70 rounded-sm"></p>
+                <div className="h-5 mr-3 animate-pulse items-center  bg-gray-200/70  rounded-full w-20 mb-2.5"></div>
               )}
             </div>
           </div>
@@ -69,8 +75,8 @@ export function CurrencyComponent({ currency }: Prop) {
           </>
         ) : (
           <>
-            <p className="mr-3 items-center truncate w-3 h-3 animate-pulse bg-gray-200/70 rounded-sm"></p>
-            <p className="mr-3 items-center truncate w-2 h-2 mt-1 animate-pulse bg-gray-200/70 rounded-sm"></p>
+            <p className="mr-3 items-center truncate w-5 h-3 animate-pulse bg-gray-200/70  rounded-full"></p>
+            <p className="mr-3 items-center truncate w-3 h-2 mt-1 animate-pulse bg-gray-200/70  rounded-full"></p>
           </>
         )}
       </div>
