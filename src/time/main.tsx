09f0a56687559d3e59e791b59c7df20a7cd4cef2
sajjad isvand <sajjadzibaee102@gmail.com>
2024-09-07 16:48:25 +0330

    fix(time): css import

--- src/time/main.tsx
+++ src/time/main.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import '../index.css'
-import './app.css'
+import './time.css'
 import { ThemeProvider } from '@material-tailwind/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
