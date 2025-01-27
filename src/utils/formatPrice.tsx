37e8f1905efbed4d6044ed3f9dc251222448e8a4
sajjad isvand <sajjadzibaee102@gmail.com>
2025-01-27 14:59:09 +0330

    fix: Update price formatting utility to handle numeric input and improve million/billion conversion

--- src/utils/formatPrice.tsx
+++ src/utils/formatPrice.tsx
@@ -1,41 +1,40 @@
 export function convertPriceToMillions(
-	price: string,
+	price: string | number,
 	lang: 'fa' | 'en',
 ): React.ReactNode {
-	const numericPrice = Number.parseFloat(price.replace(/,/g, ''))
-	if (Number.isNaN(numericPrice)) return <>{price}</>
+	const numericPrice =
+		typeof price === 'string'
+			? Number.parseFloat(price.replace(/,/g, ''))
+			: price
 
-	// Define labels for millions and billions based on language
+	if (Number.isNaN(numericPrice)) return <>{numericPrice.toLocaleString()}</>
+
 	const labels = {
 		en: { million: 'M', billion: 'B' },
 		fa: { million: 'م‌ن', billion: 'م‌د' },
 	}
 
-	// Format numbers based on language
 	const locale = lang === 'fa' ? 'fa-IR' : 'en-US'
 
 	if (numericPrice >= 1000000000) {
-		// Convert to billions
 		const billions = numericPrice / 1000000000
 		return (
 			<>
 				{billions.toLocaleString(locale, { maximumFractionDigits: 2 })}
-				<span className="text-xs">{labels[lang].billion}</span>
+				<span className="ml-[1px]">{labels[lang].billion}</span>
 			</>
 		)
 	}
 
 	if (numericPrice >= 1000000) {
-		// Convert to millions
-		const millions = numericPrice / 1000000
+		const millions = numericPrice / 10000000
 		return (
 			<>
 				{millions.toLocaleString(locale, { maximumFractionDigits: 2 })}
-				<span className="text-xs">{labels[lang].million}</span>
+				<span className="ml-[1px] text-xs">{labels[lang].million}</span>
 			</>
 		)
 	}
 
-	// If the price is less than 1 million, return the original price formatted with commas
 	return <>{numericPrice.toLocaleString(locale)}</>
 }
