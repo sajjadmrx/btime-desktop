2d3ec17154129eab64b6590a25daef68386bafdb
sajjad isvand <sajjadzibaee102@gmail.com>
2025-04-06 20:44:45 +0330

    fix(account-setting): improve loading state display with spinner and adjust container height

--- src/setting/pages/account/account.setting.tsx
+++ src/setting/pages/account/account.setting.tsx
@@ -1,3 +1,4 @@
+import { AiOutlineLoading } from 'react-icons/ai'
 import { useAuth } from '../../../context/auth.context'
 import { AuthForm } from './auth-form'
 import { UserProfile } from './user-profile'
@@ -6,10 +7,10 @@ export const AccountSetting = () => {
 	const { isAuthenticated, isLoadingUser } = useAuth()
 
 	return (
-		<div className="p-4 h-96 overflow-y-auto not-moveable font-[Vazir] scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800 scrollbar-track-transparent hover:scrollbar-thumb-gray-400 dark:hover:scrollbar-thumb-gray-700">
+		<div className="p-4 h-full overflow-y-auto not-moveable font-[Vazir] scrollbar-thin scrollbar-thumb-gray-300 dark:scrollbar-thumb-gray-800 scrollbar-track-transparent hover:scrollbar-thumb-gray-400 dark:hover:scrollbar-thumb-gray-700">
 			{isLoadingUser ? (
-				<div className="flex items-center justify-center h-64 bg-red-400">
-					در حال بارگذاری...
+				<div className="flex items-center justify-center h-full gap-2 m-auto text-lg">
+					<AiOutlineLoading className="w-5 h-5 ml-2 text-gray-500 animate-spin" />
 				</div>
 			) : isAuthenticated ? (
 				<UserProfile />
